361569493f80ad53b7beb4511fbece3a372d9f1f
Alejandro Castro <alejandroacp0628@gmail.com>
2022-11-21 01:41:10 -0500

    se implementa funcionalidades de editar contactos

--- app/src/main/java/com/example/appfuncional/db/DbContactos.java
+++ app/src/main/java/com/example/appfuncional/db/DbContactos.java
@@ -64,4 +64,50 @@ public class DbContactos extends DbHelper {
 
         return listaContactos;
     }
+    public Contactos verContacto(int id){
+
+        DbHelper dbHelper = new DbHelper(context);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+
+        Contactos contacto = null;
+        Cursor cursorContactos;
+
+        cursorContactos = db.rawQuery("SELECT * FROM " + TABLE_CONTACTOS + " WHERE id  = " + id + " LIMIT 1", null);
+
+        if(cursorContactos.moveToFirst()){
+            contacto = new Contactos();
+            contacto.setId(cursorContactos.getInt(0));
+            contacto.setNombre(cursorContactos.getString(1));
+            contacto.setTelefono(cursorContactos.getString(2));
+            contacto.setCorreo_electornico(cursorContactos.getString(3));
+
+        }
+
+        cursorContactos.close();
+
+        return contacto;
+    }
+    public boolean editarContacto(int id, String nombre, String telefono, String correo_electronico) {
+
+
+        boolean correcto = false;
+
+        DbHelper dbHelper = new DbHelper(context);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+
+        try {
+
+            db.execSQL("UPDATE "+ TABLE_CONTACTOS + " SET nombre = '"+nombre+"', telefono = '"+telefono+"', correo_electronico = '"+correo_electronico+"' WHERE id = '"+ id +"' ");
+            correcto = true;
+        } catch (Exception ex) {
+            ex.toString();
+             correcto = false;
+        }finally {
+            db.close();
+        }
+
+        return correcto;
+    }
 }
